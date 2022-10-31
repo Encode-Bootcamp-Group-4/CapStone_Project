7dfe10a3b46f19ee12a8499b88670c4b892d548a
Bryce Palichuk <bpalichu@ualberta.ca>
2022-10-30 19:01:06 -0600

    Create backend

---
+++ backend/src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+  app.enableCors(corsOptions);
+
+  const config = new DocumentBuilder()
+  .setTitle('Week 4 Team 4 Encode')
+  .setDescription('API description for Week 4 Team 4 Encode')
+  .setVersion('1.0')
+  .addTag('week-4')
+  .build(); 
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
