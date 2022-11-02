19ff4ae861ac931006a6396abe29492b43ff0767
Shawty1998 <davidsh@tcd.ie>
2022-11-02 20:42:41 +0000

    contract

--- smart-contracts/contracts/Main.sol
+++ smart-contracts/contracts/Main.sol
@@ -5,17 +5,25 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract TokenizedBallot {
 
+    event gameBegin(address indexed _from, bytes32 indexed _id, uint _value);
+
+    event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
+
     uint256 public betSize;
     //check the actial relm of possible scores this size can probably be reduced
     uint256 public highScore;
-    //the address of the current winner
-    address public currentWinner;
     //fee for the contract holder as a percentage of the betSize in bps
     uint256 private fee;
-
+    
     uint256 private feesCollected;
+    //the address of the current winner
+    address public currentWinner;
 
     address private owner;
+
+    address public playing;
+
+    bool public betsOpen;
 
     //This really is unnecesserary, should jusy be done of chain
     mapping (address => uint256) public score;
@@ -27,23 +35,30 @@ contract TokenizedBallot {
         betSize = _betSize;
         fee = _fee;
         owner = msg.sender;
+        betsOpen=true;
     }
 
     function bet() public payable {
-        //uint256 votePower_ = votePower(msg.sender);
-        //require(votePower_ >= amount, "Not enough vote power");
-		//		require(votePowerSpent[msg.sender] + amount <= votePower_, "Not enough vote power");
+        require(betsOpen,"Bets are closed :(");
+        require(msg.value >= betSize, "Need to bet BIGGER!!!!!!!!");
+        //close the betting
+        betsOpen=false;
+        playing=msg.sender;
     }
 
 
-    function saveScore(uint256 _score) public {
+    function saveScore(uint256 _score) external {
+        require(msg.sender == owner,
+         "This function can only be accessed through game");
         if (_score > highScore) {
             highScore = _score;
-            score[msg.sender] = _score;
-            prize[msg.sender] += ( betSize * ( 10000-fee ) ) / 10000;
+            score[playing] = _score;
+            prize[playing] += ( betSize * ( 10000-fee ) ) / 10000;
         } else {
             prize[currentWinner] += ( betSize * ( 10000-fee ) ) / 10000;
         }
+        playing = address(0);
+        betsOpen = true;
     }
 
     /// @notice Withdraw `amount` from that accounts prize pool
