139b57a4539f7d0b877a04de2878eacbd21c8513
Shawty1998 <davidsh@tcd.ie>
2022-11-02 17:25:58 +0000

    contract

--- smart-contracts/contracts/Main.sol
+++ smart-contracts/contracts/Main.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-
+//import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenizedBallot {
 
@@ -13,6 +13,10 @@ contract TokenizedBallot {
     //fee for the contract holder as a percentage of the betSize in bps
     uint256 private fee;
 
+    uint256 private feesCollected;
+
+    address private owner;
+
     //This really is unnecesserary, should jusy be done of chain
     mapping (address => uint256) public score;
 
@@ -21,7 +25,8 @@ contract TokenizedBallot {
 
     constructor(uint256 _betSize, uint256 _fee) {
         betSize = _betSize;
-        fee = _fee;      
+        fee = _fee;
+        owner = msg.sender;
     }
 
     function bet() public payable {
@@ -40,4 +45,19 @@ contract TokenizedBallot {
             prize[currentWinner] += ( betSize * ( 10000-fee ) ) / 10000;
         }
     }
+
+    /// @notice Withdraw `amount` from that accounts prize pool
+    function prizeWithdraw(uint256 amount) public {
+        require(amount <= prize[msg.sender], "Not enough prize");
+        prize[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+
+    /// @notice Withdraw `amount` from the owner pool
+    function ownerWithdraw(uint256 amount) public {
+        require(amount <= feesCollected, "Not enough fees collected");
+        feesCollected -= amount;
+        payable(owner).transfer(amount);
+    }
+        
 }
