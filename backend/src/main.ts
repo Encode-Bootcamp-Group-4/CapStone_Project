e1e9f47bbcbeb3609ef6084f606e44e506cfdc2a
Marco Germanier <marco@mrgt.me>
2022-11-04 21:04:15 +0000

    testing boilerplate

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,30 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    allowedHeaders: "Content-Type, Accept, Authorization",
   };
   app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
-  .setTitle('Week 4 Team 4 Encode')
-  .setDescription('API description for Week 4 Team 4 Encode')
-  .setVersion('1.0')
-  .addTag('week-4')
-  .build(); 
+    .setTitle("Proof of Snake API")
+    .setDescription("SSSSSSSSSSSSSSSSssssssssssSSSSSSSSss")
+    .setVersion("0.1")
+    .addTag("Group 4 Encode Capstone Project")
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
   await app.listen(3000);
 
   if (module.hot) {
