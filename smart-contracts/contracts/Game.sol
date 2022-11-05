4217a5eaea9e1d425bd8ea2751adf30bfeafca59
Marco Germanier <marco@mrgt.me>
2022-11-05 22:48:06 +0000

    finished rewriting contarct

--- smart-contracts/contracts/Game.sol
+++ smart-contracts/contracts/Game.sol
@@ -4,6 +4,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Game is Ownable {
+    uint256 public minBet;
+    address public ownerAddress;
+
     constructor(uint256 _minBet) {
         minBet = _minBet;
         ownerAddress = msg.sender;
@@ -23,25 +26,24 @@ contract Game is Ownable {
         address addressSetter;
         address addressChallenger; // is the game being challenged right now?
     }
-    uint256 public gameId;
+
     // to store active games in contract
-    mapping(gameId => gameStruct) public games;
+    mapping(uint256 => gameStruct) public games;
 
     // create a game
-    function createGame() public payable {
+    function createGame() public payable returns (uint256) {
         require(msg.value >= minBet, "need to bet some money my friend");
+
         _gameIds.increment();
         uint256 newGameId = _gameIds.current();
 
-        const _gameStruct = gameStruct(
+        games[newGameId] = gameStruct(
             newGameId,
             msg.value,
             0,
             msg.sender,
-            0x0
+            0x0000000000000000000000000000000000000000
         );
-        games[newGameId] = _gameStruct;
-        emit GameCreated(_gameStruct);
         return newGameId;
     }
 
@@ -50,7 +52,8 @@ contract Game is Ownable {
         uint256 _gameId,
         uint16 _score,
         address _address
-    ) public Ownable {
+    ) public {
+        require(msg.sender == ownerAddress, "onlyOwner!");
         require(
             _address == games[_gameId].addressSetter,
             "Incorrect address for game id!"
@@ -60,18 +63,20 @@ contract Game is Ownable {
     }
 
     // challenge an existing game
-    function startChallenge(uint256 _gameid) public payable {
-        const _game = games[_gameId];
+    function startChallenge(uint256 _gameId) public payable {
+        gameStruct memory _game = games[_gameId];
         require(
-            _game.addressChallenger == 0x0 && _game.score > 0,
+            _game.addressChallenger ==
+                0x0000000000000000000000000000000000000000 &&
+                _game.score > 0,
             "game is not open!"
         );
-        require(msg.value >= _game.betSize, "You did not match the bet");
+        require(msg.value == _game.betSize, "You did not match the bet");
         require(
             _game.addressSetter != msg.sender,
             "You cannot challenge yourself!"
         );
-        games[_gameId].gameChallenger = msg.sender;
+        games[_gameId].addressChallenger = msg.sender;
     }
 
     // close off the challenge. backend function
@@ -79,34 +84,36 @@ contract Game is Ownable {
         uint256 _gameId,
         uint16 _score,
         address _address
-    ) public Ownable {
+    ) public {
+        require(msg.sender == ownerAddress, "onlyOwner!");
         require(
             _address == games[_gameId].addressChallenger,
             "address is not the challenger!"
         );
-        _game = games[_gameId];
+        gameStruct memory _game = games[_gameId];
         if (_score > _game.score) {
             // destroy the game
             delete games[_gameId];
             // add balance to winner
-            balances[_address] = balances[_address] + _game.betSize;
+            balances[_address] = balances[_address] + _game.betSize * 2;
         } else {
             // add balance to winner
             balances[_game.addressSetter] =
                 balances[_game.addressSetter] +
                 _game.betSize;
             // open game to others
-            games[_gameId].addressChallenger = 0x0;
+            games[_gameId]
+                .addressChallenger = 0x0000000000000000000000000000000000000000;
         }
     }
 
     /// @notice Withdraw `amount` from that accounts prize pool
     function prizeWithdraw(uint256 _amount) public {
         require(_amount <= balances[msg.sender], "Not enough balance");
-        prize[msg.sender] -= _amount;
+        balances[msg.sender] -= _amount;
         // check with bryce and with mw that this is safe
-        payable(msg.sender).transfer(_amount * 0.95);
+        payable(msg.sender).transfer((_amount * 19) / 20);
         // our fee - 5% at extraction
-        payable(ownerAddress).transfer(_amount * 0.05);
+        payable(ownerAddress).transfer(_amount / 20);
     }
 }
