3e88f79a98b3708f74e38b65d13f18d8affd4ae9
Marco Germanier <marco@mrgt.me>
2022-11-06 13:29:00 +0000

    set up backend for development

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Query } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post("set_game_score")
+  setGameScore(
+    @Query("gameId") _gameId: number,
+    @Query("score") _score: number,
+    @Query("address") _address: string
+  ): Promise<string> {
+    return this.appService.setGameScore(_gameId, _score, _address);
+  }
+
+  @Post("close_challenge")
+  closeChallenge(
+    @Query("gameId") _gameId: number,
+    @Query("score") _score: number,
+    @Query("address") _address: string
+  ): Promise<string> {
+    return this.appService.closeChallenge(_gameId, _score, _address);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ethers } from "ethers";
+import GameJSON from "../assets/Game.sol/Game.json";
+
+const GAME_ADDRESS = "";
+const GAME_ABI = GameJSON.abi;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  // init
+  provider: ethers.providers.Provider;
+  gameContract: ethers.Contract;
+  gameContractConnect: ethers.Contract;
+
+  constructor() {
+    this.provider = ethers.getDefaultProvider("goerli");
+    this.gameContract = new ethers.Contract(
+      GAME_ADDRESS,
+      GAME_ABI,
+      this.provider
+    );
+    const pkey = process.env.PRIVATE_KEY;
+    const wallet = new ethers.Wallet(pkey, this.provider);
+    const signer = wallet.connect(this.provider);
+    this.gameContractConnect = this.gameContract.connect(signer);
+  }
+
+  async setGameScore(
+    _gameId: number,
+    _score: number,
+    _address: string
+  ): Promise<string> {
+    if (_score > 625) {
+      _score = 1;
+    }
+    const tx = await this.gameContractConnect.setGameScore(
+      _gameId,
+      _score,
+      _address
+    );
+    return tx;
+  }
+
+  async closeChallenge(
+    _gameId: number,
+    _score: number,
+    _address: string
+  ): Promise<string> {
+    if (_score > 625) {
+      _score = 1;
+    }
+    const tx = await this.gameContractConnect.closeChallenge(
+      _gameId,
+      _score,
+      _address
+    );
+    return tx;
   }
 }
